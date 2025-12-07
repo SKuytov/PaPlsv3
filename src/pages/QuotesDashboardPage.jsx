47aaa07790c59aad222569f598616c7a35caf3a9
SKuytov <138648456+SKuytov@users.noreply.github.com>
2025-12-07 20:07:18 +0200

    feat: add Quotes Dashboard page

---
+++ src/pages/QuotesDashboardPage.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import QuotesDashboard from '@/components/modules/quotes/QuotesDashboard';
+
+const QuotesDashboardPage = () => {
+  return <QuotesDashboard />;
+};
+
+export default QuotesDashboardPage;
