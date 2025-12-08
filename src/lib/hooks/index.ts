157688ae7ea5d7363d913e693dfe824c1b6f71a6
SKuytov <138648456+SKuytov@users.noreply.github.com>
2025-12-08 21:40:57 +0200

    Create hooks index file for centralized exports

---
+++ src/lib/hooks/index.ts
@@ -0,0 +1,8 @@
+// Quote-related hooks
+export { useQuoteItems } from './useQuoteItems';
+export type { QuoteItem } from './useQuoteItems';
+
+export { useQuoteEmailBody } from './useQuoteEmailBody';
+export type { QuoteEmailParams } from './useQuoteEmailBody';
+
+export { useSupplierPartMapping } from './useSupplierPartMapping';
