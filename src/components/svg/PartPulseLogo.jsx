ca4a032dba4a11ea0ef4880ba1a055a7fb754d6a
SKuytov <138648456+SKuytov@users.noreply.github.com>
2026-01-05 18:57:40 +0200

    Add new SVG logo to PartPulseLogo component

--- src/components/svg/PartPulseLogo.jsx
+++ src/components/svg/PartPulseLogo.jsx
@@ -14,42 +14,71 @@
 
 const PartPulseLogo = () => {
   return (
-    <svg
-      width="100%"
-      height="100%"
-      viewBox="0 0 100 100"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      className="w-8 h-8"
-    >
-      {/* Placeholder Professional Logo - Replace with your custom SVG */}
-      
-      {/* Main circle background */}
-      <circle cx="50" cy="50" r="48" fill="#14b8a6" opacity="0.1" stroke="#14b8a6" strokeWidth="2" />
-      
-      {/* Outer gradient circle */}
-      <circle cx="50" cy="50" r="42" fill="#14b8a6" />
-      
-      {/* Inner lighter circle for depth */}
-      <circle cx="50" cy="50" r="38" fill="#0d9488" />
-      
-      {/* Pulse waves - representing "Pulse" */}
-      <circle cx="50" cy="50" r="18" fill="none" stroke="#ffffff" strokeWidth="2.5" opacity="0.8" />
-      <circle cx="50" cy="50" r="26" fill="none" stroke="#ffffff" strokeWidth="1.5" opacity="0.5" />
-      
-      {/* Center dot */}
-      <circle cx="50" cy="50" r="4" fill="#ffffff" />
-      
-      {/* Stylized "P" shape in white - subtle branding */}
-      <path
-        d="M 42 38 L 42 62 M 42 45 L 52 45 Q 56 45 56 49 Q 56 53 52 53 L 42 53"
-        stroke="#ffffff"
-        strokeWidth="2.5"
-        fill="none"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-    </svg>
+    <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 254.55 73.08">
+  <defs>
+    <style>
+      .cls-1 {
+        fill: url(#linear-gradient);
+      }
+
+      .cls-1, .cls-2, .cls-3, .cls-4 {
+        stroke-width: 0px;
+      }
+
+      .cls-5 {
+        font-family: Roboto-Light, Roboto;
+        font-size: 10.8px;
+        font-weight: 300;
+      }
+
+      .cls-5, .cls-6 {
+        fill: #2d3748;
+      }
+
+      .cls-7 {
+        letter-spacing: .02em;
+      }
+
+      .cls-2 {
+        fill: #1e3a5f;
+      }
+
+      .cls-3 {
+        fill: #f36b32;
+      }
+
+      .cls-6 {
+        font-family: Roboto-Black, Roboto;
+        font-size: 33.91px;
+        font-weight: 800;
+      }
+
+      .cls-4 {
+        fill: #154577;
+      }
+
+      .cls-8 {
+        letter-spacing: 0em;
+      }
+    </style>
+    <linearGradient id="linear-gradient" x1="0" y1="36.14" x2="93.84" y2="36.14" gradientUnits="userSpaceOnUse">
+      <stop offset=".46" stop-color="#1e3a5f"/>
+      <stop offset=".47" stop-color="#313e5a"/>
+      <stop offset=".47" stop-color="#654a4f"/>
+      <stop offset=".48" stop-color="#b75d3e"/>
+      <stop offset=".48" stop-color="#f36b32"/>
+    </linearGradient>
+  </defs>
+  <path class="cls-4" d="M25.97,32.89h-11.94l-3.22-2.67,2.03-7.88,7.04.6s.84-2.37,1.69-3.63c.71-1.05,2.5-3.13,2.5-3.13l-3.35-5.11s-.02-.1.02-.13l6.72-5.59c.29-.24.71-.22.98.04l4.55,4.25s1.92-1.25,3.63-1.92c1.53-.6,3.78-1.09,3.78-1.09L40.57,0h5.78v14.34s-6.87-.08-12.75,5.43c-7.87,7.37-7.63,13.13-7.63,13.13Z"/>
+  <path class="cls-4" d="M69.53,32.89h11.94s3.22-2.67,3.22-2.67l-2.03-7.88-7.04.6s-.84-2.37-1.69-3.63c-.71-1.05-2.5-3.13-2.5-3.13l3.35-5.11s.02-.1-.02-.13l-6.72-5.59c-.29-.24-.71-.22-.98.04l-4.55,4.25s-1.92-1.25-3.63-1.92c-1.53-.6-3.78-1.09-3.78-1.09L54.93,0h-5.78s0,14.34,0,14.34c0,0,6.87-.08,12.75,5.43,7.87,7.37,7.63,13.13,7.63,13.13Z"/>
+  <path class="cls-3" d="M69.53,32.89h11.94s3.22-2.67,3.22-2.67l-2.03-7.88-7.04.6s-.84-2.37-1.69-3.63c-.71-1.05-2.5-3.13-2.5-3.13l3.35-5.11s.02-.1-.02-.13l-6.72-5.59c-.29-.24-.71-.22-.98.04l-4.55,4.25s-1.92-1.25-3.63-1.92c-1.53-.6-3.78-1.09-3.78-1.09L54.93,0h-5.78s0,14.34,0,14.34c0,0,6.87-.08,12.75,5.43,7.87,7.37,7.63,13.13,7.63,13.13Z"/>
+  <path class="cls-2" d="M25.97,32.89h-11.94l-3.22-2.67,2.03-7.88,7.04.6s.84-2.37,1.69-3.63c.71-1.05,2.5-3.13,2.5-3.13l-3.35-5.11s-.02-.1.02-.13l6.72-5.59c.29-.24.71-.22.98.04l4.55,4.25s1.92-1.25,3.63-1.92c1.53-.6,3.78-1.09,3.78-1.09l.18-6.64h5.78v14.34s-6.87-.08-12.75,5.43c-7.87,7.37-7.63,13.13-7.63,13.13Z"/>
+  <path class="cls-3" d="M69.53,40.19h11.94s3.22,2.67,3.22,2.67l-2.03,7.88-7.04-.6s-.84,2.37-1.69,3.63c-.71,1.05-2.5,3.13-2.5,3.13l3.35,5.11s.02.1-.02.13l-6.72,5.59c-.29.24-.71.22-.98-.04l-4.55-4.25s-1.92,1.25-3.63,1.92c-1.53.6-3.78,1.09-3.78,1.09l-.18,6.64h-5.78s0-14.34,0-14.34c0,0,6.87.08,12.75-5.43,7.87-7.37,7.63-13.13,7.63-13.13Z"/>
+  <path class="cls-2" d="M25.97,40.19h-11.94l-3.22,2.67,2.03,7.88,7.04-.6s.84,2.37,1.69,3.63c.71,1.05,2.5,3.13,2.5,3.13l-3.35,5.11s-.02.1.02.13l6.72,5.59c.29.24.71.22.98-.04l4.55-4.25s1.92,1.25,3.63,1.92c1.53.6,3.78,1.09,3.78,1.09l.18,6.64h5.78v-14.34s-6.87.08-12.75-5.43c-7.87-7.37-7.63-13.13-7.63-13.13Z"/>
+  <path class="cls-1" d="M51.18,54.96l-7.24-27.72-3.83,11.13-40.11-.36v-1.83l38.25-.32,5.94-18.63c.06-.2.35-.19.4.01l7.12,28.94,3.29-10.5,38.85.46v1.96l-36.88.28-5.18,16.58c-.09.3-.52.3-.6,0Z"/>
+  <text class="cls-6" transform="translate(102.95 48.73)"><tspan class="cls-8" x="0" y="0">P</tspan><tspan x="21.89" y="0">a</tspan><tspan class="cls-7" x="39.89" y="0">r</tspan><tspan x="53.57" y="0">tPulse</tspan></text>
+  <text class="cls-5" transform="translate(241.9 60.94)"><tspan x="0" y="0">V3</tspan></text>
+</svg>
   );
 };
 
